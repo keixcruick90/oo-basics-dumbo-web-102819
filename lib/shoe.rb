8c72043ef34034a3cbdab50acde0b0d4089a5eb0
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-29 18:06:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Shoe
+
+  attr_accessor :size, :color, :material
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def condition
+    puts "New"
+  end
+
+end
